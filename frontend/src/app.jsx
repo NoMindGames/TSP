e6672cb1cd9a0ddeecdc51770d6d11b3bff8b736
Darknessich <blakibrown@gmail.com>
2024-03-13 23:08:03 +0300

    frontend struct

---
+++ frontend/src/app.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './app.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <img className="app-logo" alt="logo" />
+        <p>
+          Edit <code>src/app.js</code> and save to reload.
+        </p>
+        <a
+          className="app-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
