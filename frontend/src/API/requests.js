d891c8091655343477e15c5c5d16c44e0621366a
Darknessich <blakibrown@gmail.com>
2024-03-29 17:53:26 +0300

    add healthcheck

--- frontend/src/API/requests.js
+++ frontend/src/API/requests.js
@@ -1,7 +1,7 @@
-const SERVER_HOST = '127.0.0.1:8000';
+const SERVER_HOST = '127.0.0.1:8040';
 
 const serverAPI = {
-  healthCheck: `http://${SERVER_HOST}/healthcheck`
+  healthCheck: `http://${SERVER_HOST}/healthcheck/`
 };
 
 function getHealthCheck(successCb, errorCb, finalyCb) {
@@ -14,7 +14,7 @@ function getHealthCheck(successCb, errorCb, finalyCb) {
       const { statusText, status } = response;
       throw new Error(`${status} - ${statusText}`);
     })
-    .then((response) => response.json)
+    .then((response) => response.json())
     .then((data) => successCb(data))
     .catch((error) => errorCb(error.message))
     .finally(finalyCb);
